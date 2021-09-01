bd3bfab02a8356ca211c1fd4b06040aabbcddbf3
lrbn86 <lrbn86@gmail.com>
2021-09-01 08:51:05 -0400

    New layout/style

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 const wrapper = $(".wrapper");
 wrapper.hide();
-wrapper.fadeIn(1000);
+const hero = $(".hero").children();
+hero.hide();
+const nav = $(".nav");
+
+wrapper.slideDown(800).promise().done(() => {
+  hero.each(function(index) {
+    $(this).slideDown(800);
+  }).promise().done(function() {
+    nav.animate({"right": "5rem"});
+  });
+});
+
+
