6a8023df0156227e6a57e51aa34b0320202006c1
lrbn86 <lrbn86@gmail.com>
2021-09-02 03:25:19 -0400

    Remove scroll snapping for better UX

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
 const wrapper = $(".wrapper");
 wrapper.hide();
-const hero = $(".hero").children();
-hero.hide();
-const nav = $(".nav");
-
-wrapper.slideDown(800).promise().done(() => {
-  hero.each(function(index) {
-    $(this).slideDown(800);
-  }).promise().done(function() {
-    nav.animate({"right": "5rem"});
-  });
-});
-
+wrapper.slideDown(1000);
 
