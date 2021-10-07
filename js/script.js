fab20cf0a221d4ecc752d44f2a47f0ba662590a9
lrbn86 <lrbn86@gmail.com>
2021-10-06 20:45:29 -0400

    Revised home page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-$('.menu-btn').click(() => {
-  $('.nav-container').fadeToggle();
-  $('body').addClass('noscroll');
+// Handle mobile navigation interaction
+const openMenuBtn = $('#open-menu-btn');
+const closeMenuBtn = $('#close-menu-btn');
+const navContainer = $('#nav-container');
+
+openMenuBtn.click(() => {
+  navContainer.show();
+  openMenuBtn.hide();
+  closeMenuBtn.show();
 });
 
-$('.close-menu-btn').click(() => {
-  $('.nav-container').fadeToggle();
-  $('body').removeClass('noscroll');
-})
+closeMenuBtn.click(() => {
+  navContainer.hide();
+  closeMenuBtn.hide();
+  openMenuBtn.show();
+});
+
+// Listen for window resize event
+// If we are in desktop view, always show the navigation
+window.addEventListener("resize", () => {
+  if (window.matchMedia("(min-width: 1024px)").matches) {
+    navContainer.show();
+  }
+});
